736d16ba752c8139aa0ac84de3c66c074e423feb
Jonas <jpcyvas@gmail.com>
2026-09-13 14:55:31 -0500

    Update year for schedule

--- src/football-bot.js
+++ src/football-bot.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const footballApiUrlStandings = 'https://site.api.espn.com/apis/v2/sports/football/nfl/standings';
-const footballApiUrlSchedule = 'https://cdn.espn.com/core/nfl/schedule?xhr=1&year=2025&week={WEEK}';
+const footballApiUrlSchedule = 'https://cdn.espn.com/core/nfl/schedule?xhr=1&year=2026&week={WEEK}';
 
 //Get today's date in YYYY-MM-DD format
 function getTodaysDate() {
